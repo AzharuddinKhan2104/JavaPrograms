3d12007df4a18f7346b151363981b7cf9faa447a
AzharuddinKhan2104 <azharuddin_khan@yahoo.com>
2023-09-20 10:51:09 +0530

    Programs add two integers

---
+++ src/test/java/basics/AddTwoIntegers.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+     addTwoIntegers(2,3);
+    }
+    static void addTwoIntegers(int number1 , int number2){
+        int result = number1 + number2 ;
+        System.out.println("Sum of Two Numbers is " + result);
+    }
+
+}
